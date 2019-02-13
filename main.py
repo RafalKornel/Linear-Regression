edda1c07b6796810c39e20c7aed8636a2114165a
RafalKornel <rafal.kornel123@gmail.com>
2019-02-13 14:20:00 +0100

    Cleaned code a little bit

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ import imageio
 from model import Model
 from matplotlib import pylab as pl
 
-# Parameters:
 
+# Parameters:
 epochs = 1000
 learning_rate = 0.001
 visualization = True
 snap_freq = 10
 batch_size = 5
 
+
 # Load file and assign features and lables
 _file = pl.loadtxt('input_file.txt')
 x = _file[:, 0]
 y = _file[:, 1]
+
 
 # Randomly generate slope and bias values
 rand_a = random.uniform(-10, 10)
@@ -31,18 +33,14 @@ print("Randomly generated slope:{0} and bias:{1} ".format(rand_a, rand_b))
 test = Model(x, y, learning_rate, batch_size, visualization, epochs, bias = rand_b, slope = rand_a, frequency = snap_freq)
 test.learn()
 
-#print(test.pick_random_batch(4))
-print("Result slope:{0} and bias:{1} ".format(test.slope, test.bias))
 
 # Visualization - generating plot and gif file
 f = lambda x, a, b: a * x + b
-pl.plot(x, y, 'o')
-_x = pl.arange(-10, 10, 0.1)
-pl.plot(_x, f(_x, test.slope, test.bias))
-
-pl.show()
 
 if visualization == True:
+    print("Result slope:{0} and bias:{1} ".format(test.slope, test.bias))
+    test.plot_model(show_plot = True)
+
     images = []
     for filename in sorted(os.listdir('shots/'), key = lambda x : int(x[0:-4:])):
         images.append(imageio.imread("shots/{0}".format(filename)))
--- model.py
+++ model.py
@@ -22,21 +22,26 @@ class Model():
         self.frequency = frequency
         self.visualize = visualize
 
+
     def __str__(self):
         return "slope: {0} | bias: {1}".format(self.slope, self.bias)
+
 
     def predict(self,x):
         ''' Applies given x value to current trained linear function '''
         return self.slope * x + self.bias
 
+
     def calc_square_error(self, entry):
         ''' Calculates square error for given entry '''
         return (entry - self.predict(entry))**2
+
 
     def calc_error_on_batch(self, batch):
         ''' Calculate average error value over given batch '''
         _sum = sum( [self.calc_square_error(x) for x in batch] )
         return _sum / len(batch)
+
 
     def calc_gradient_slope(self, x, y, use_batch = False):
         ''' Calculate slope gradient, i.e. partial derivative over slope variable in given point, returned
@@ -51,6 +56,7 @@ class Model():
         else:
             return [2 * x**2, 2 * x + self.bias - 2 * x * x]
 
+
     def calc_gradient_bias(self, x, y, use_batch = False):
         ''' Calculate bias gradient, i.e. partial derivative over bias variable in given point'''
         if use_batch:
@@ -62,30 +68,41 @@ class Model():
         else:
             return [2, 2 * self.slope * x - 2 * y]
 
+
     def pick_random_batch(self, length):
         ''' Returns random values from model's features '''
         return [np.random.randint(0, len(self.features)) for i in range(length)]
         #return np.random.choice(self.features, length)
 
+
     def deternime_next_step_slope(self, gradient_slope):
         ''' Determines how much should slope value change in this iteration '''
         return (self.slope * gradient_slope[0] + gradient_slope[1]) * self.learning_step
+
 
     def determine_next_step_bias(self, gradient_bias):
         ''' Determines how much should bias value change in this iteration '''
         return (self.bias * gradient_bias[0] + gradient_bias[1]) * self.learning_step
 
-    def export_model_to_png(self, iteration):
-        ''' Creates series of plots in png format, later converted to gif animation for visualization '''
-        if not os.path.exists('shots/'): os.mkdir('shots/')
+
+    def plot_model(self, iteration = 0, save_plot = False, show_plot = False):
+        ''' Creates plot in matplotlib's pylab module, if save_plot = True, saves plot to shots folder, if show_plot = True, shows plot (opens pylab window with plot '''
         pl.plot(_x, f(_x, self.slope, self.bias))
         pl.plot(self.features, self.labels, 'k.')
         pl.grid(True)
         axes = pl.gca()
         axes.set_xlim([-10, 10])
         axes.set_ylim([-10, 10])
-        pl.savefig("shots/{0}".format(iteration), bbox_inches = 'tight')
+
+        if save_plot:
+            if not os.path.exists('shots/'): os.mkdir('shots/')
+            pl.savefig("shots/{0}".format(iteration), bbox_inches = 'tight')
+
+        if show_plot:
+            pl.show()
+
         pl.clf()
+
 
     def learn(self):
         for epoch in range(self.epochs):
@@ -105,4 +122,4 @@ class Model():
 
             if self.visualize:
                 if epoch % self.frequency == 0:
-                    self.export_model_to_png(iteration = epoch)
+                    self.plot_model(iteration = epoch, save_plot = True)#export_model_to_png(iteration = epoch)
